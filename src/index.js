33a767ee2c89950890ca7a0c9414293b4ad70bf0
Benjamin Kwok <benjaminkwokhuen@gmail.com>
2017-05-26 19:16:17 +0800

    init commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
+// import libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// import component from AntDesign
+import { Button } from 'antd';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component
+class App extends React.Component {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  // API fetching logic
+  APIcaller() {
+
+    https://api.dev.pepwave.com/api/oauth2/auth?client_id=ba3c3c5c3c81af715e9bc9fc2b96e363&response_type=[code/token]&redirect_uri=[REDIRECT_URI]
+    fetch('https://api1.dev.pepwave.com/rest/o/riMA5x/g', {
+      credentials: 'include',
+      method: 'get'
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    }, (error) => {
+      console.log("Something Went wrong", error);
+    })
+    .then((data) => {
+      var dummyData = require("./../dummyData.txt");
+      console.log("Data being fetched");
+    })
+    .catch(err => console.log("Unexpected error", err));
+  }
+
+  render () {
+    return <Button onClick={this.APIcaller}>Michaelsofts Dev Inc</Button>
+  }
+}
+
+
+// Take a component and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
